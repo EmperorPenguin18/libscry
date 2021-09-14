11c8afded0ff504adcbe6f367aa7ca93ae48e618
Sebastien MacDougall-Landry <60635017+EmperorPenguin18@users.noreply.github.com>
2021-09-14 14:16:43 -0400

    Update and rename scry.cc to libscry.cc

---
+++ libscry.cc
@@ -0,0 +1,30 @@
+#include <libscry.h>
+#include <iostream>
+#include <curl/curl.h>
+#include <sqlite3.h>
+
+using namespace std;
+curl_global_init(CURL_GLOBAL_ALL);
+
+extern "C" MyClass* create_object()
+{
+  return new MyClass;
+}
+
+extern "C" void destroy_object( MyClass* object )
+{
+  delete object;
+}
+
+MyClass::MyClass()
+{
+  easyhandle = curl_easy_init();
+}
+
+void MyClass::DoSomething()
+{
+  curl_easy_setopt(easyhandle, CURLOPT_URL, "https://api.scryfall.com/cards/search?q=c%3Ared+pow%3D3");
+  success = curl_easy_perform(easyhandle);
+}
+
+curl_global_cleanup();
