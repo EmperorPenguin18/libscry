12223378d59dc8f4c75762604c16ae66315986df
Sebastien MacDougall-Landry <60635017+EmperorPenguin18@users.noreply.github.com>
2021-10-03 19:28:55 -0400

    Update libscry.cc

--- libscry.cc
+++ libscry.cc
@@ -2,31 +2,79 @@
 #include <iostream>
 #include <curl/curl.h>
 #include <sqlite3.h>
+#include <string>
+#include <json/json.h>
+#include <sstream>
 
 using namespace std;
 
-extern "C" MyClass* create_object()
+extern "C" Scry* create_object()
 {
-  return new MyClass;
+  return new Scry;
 }
 
-extern "C" void destroy_object( MyClass* object )
+extern "C" void destroy_object( Scry* object )
 {
-  curl_global_cleanup();
   delete object;
 }
 
-MyClass::MyClass()
+Scry::Scry()
 {
   curl_global_init(CURL_GLOBAL_ALL);
   easyhandle = curl_easy_init();
   rc = sqlite3_open("/home/sebastien/test.db", &db);
 }
 
-void MyClass::DoSomething()
+static string *DownloadedResponse;
+
+static size_t writer(char *data, size_t size, size_t nmemb, string *buffer_in)
 {
-  curl_easy_setopt(easyhandle, CURLOPT_URL, "https://api.scryfall.com/cards/named?fuzzy=island");
-  success = curl_easy_perform(easyhandle);
+    cout << "In writer callback" << endl;
+
+    // Is there anything in the buffer?
+    if (buffer_in != NULL)
+    {
+        cout << "Buffer not null" << endl;
+        // Append the data to the buffer
+        buffer_in->append(data, size * nmemb);
+        cout <<" Buffer appended, seting response" << endl;
+
+        DownloadedResponse = buffer_in;
+        cout << "Set downloadedResponse" << endl;
+        return size * nmemb;
+    }
+
+    return 0;
+}
+
+Card Scry::cards_named(string search)
+{
+  curl_easy_setopt(easyhandle, CURLOPT_URL, "https://api.scryfall.com/cards/named?fuzzy=" + search);
+  curl_easy_setopt(easyhandle, CURLOPT_WRITEFUNCTION, writer);
+  string *data;
+  curl_easy_setopt(easyhandle, CURLOPT_WRITEDATA, data);
+  CURLcode success = curl_easy_perform(easyhandle);
+  if (success != 0) {
+    printf("Error");
+    exit(1);
+  }
+  stringstream temp(&data);
+  Json::Value root;
+  temp >> root;
+  Card card(root["name"]);
+  return card
+}
+
+void Scry::cleanup()
+{
   curl_easy_cleanup(easyhandle);
+  curl_global_cleanup();
   sqlite3_close(db);
+}
+
+Card::Card(string name) { name(name) }
+
+string Card::getName()
+{
+  return name;
 }
